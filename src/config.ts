41269d5098c0a707fb4fafd60bc43d4a66e11553
ozmouse <gloriapozner@gmail.com>
2025-04-26 23:21:12 +0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
+import configJson from '../config.json';
+
 export const config = {
-  development: false,
-  debug: true,
-  appKey: "crash-0.1.0",
-  api: `${process.env.REACT_APP_API_URL}/api`,
-  wss: process.env.REACT_APP_API_URL as string,
+  development: configJson.development,
+  debug: configJson.debug,
+  appKey: configJson.appKey,
 };
